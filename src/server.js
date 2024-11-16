ee862cddfa196cec65d367f2e2dc3f1b4529debc
Ricardo Vega <vegagarrido@gmail.com>
2024-11-16 16:04:38 -0600

    adding compression & limiter

--- src/server.js
+++ src/server.js
@@ -3,9 +3,31 @@ import helmet from 'helmet';
 
 import mainRoutes from './main.routes';
 import userRoutes from './user.routes';
+import { rateLimit } from 'express-rate-limit';
+import compression from 'compression';
 
 const app = express();
 const port = 3001;
+
+const limiter = rateLimit({
+	windowMs: 5 * 60 * 1000, // 5 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+
+/**
+ * Middleware to compress the HTTP response.
+ * This will reduce the size of the response body and improve performance by sending compressed data to the client.
+ * It uses Gzip or Brotli compression based on the client's supported encoding.
+ */
+//app.use(compression);
+
+/**
+ * Apply the rate limiting middleware to all requests.
+ */
+app.use(limiter);
 
 /**
  * Middleware to parse incoming requests with JSON payloads.
